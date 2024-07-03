a0cfdd049848ef43b5aa2d7251fe2178db5537c5
Raffaele Maisto <far1987@libero.it>
2024-07-03 15:04:19 +0200

    adds starter security dependency, disables csrf policy for post method

---
+++ src/main/java/com/corsificio/configuration/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.corsificio.configuration;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig  {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		return http.csrf().disable().build();
+	}
+}
